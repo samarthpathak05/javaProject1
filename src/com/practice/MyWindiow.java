7ded4395575b4408bb785b17cb65e1b6d3ae4007
Samarth pathak <samarthpathak@Samarths-iMac.local>
2023-10-08 23:28:09 +0530

    Initial commit

---
+++ src/com/practice/MyWindiow.java
@@ -0,0 +1,56 @@
+package com.practice;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyWindiow extends JFrame {
+    private JLabel heading;
+
+    private JLabel clockLabel;
+
+    private Font font=new Font("", Font.BOLD, 35);
+    MyWindiow(){
+        super.setTitle("My Clock");
+        super.setSize(400, 400);
+        super.setLocation(300, 50);
+        this.createGUI();
+        this.startClock();
+        super.setVisible(true);
+
+    }
+
+    public void createGUI(){
+        //gui
+        heading= new JLabel("My Clock");
+        clockLabel = new JLabel("clock");
+
+        heading.setFont(font);
+        clockLabel.setFont(font);
+
+        this.setLayout(new GridLayout(2,1));
+        this.add(heading);
+        this.add(clockLabel);
+
+
+    }
+    public void startClock(){
+           Timer timer=new Timer(1000, new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+ //                String dateTime=new Date().toString();
+  //               String dateTime=new Date().toLocaleString();
+
+                   Date d=new Date();
+
+                   SimpleDateFormat sfd=new SimpleDateFormat("hh: mm: ss");
+                   String dateTime= sfd.format(d);
+                 clockLabel.setText(dateTime);
+               }
+           });
+          timer.start();
+    }
+}
